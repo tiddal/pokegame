3d7627273d9ca4620a0c9295a5541e9a3d58f8d2
tiddal <tiddal96@gmail.com>
2020-08-14 19:33:11 +0100

    Updates

---
+++ precache-manifest.85dc894856b872f6f0fd6ca06f243b1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3045564e13fbff668fc256224aa5927a",
+    "url": "/pokegame/index.html"
+  },
+  {
+    "revision": "b644b71a3c3d7d1a2cdf",
+    "url": "/pokegame/static/js/2.3172a7c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokegame/static/js/2.3172a7c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571df4fb82d2492f0907",
+    "url": "/pokegame/static/js/main.7cb0944b.chunk.js"
+  },
+  {
+    "revision": "e3a1ab243f16e884d869",
+    "url": "/pokegame/static/js/runtime-main.251c39f0.js"
+  }
+]);
